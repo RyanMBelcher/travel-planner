652106b97f50edc10dec5e4f155f2917f90cf463
Ryan Belcher <ryanbelcher@ryans-air.myfiosgateway.com>
2022-12-20 11:10:18 -0500

    DTR

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,7 +4,12 @@ const Trip = require('./Trip');
 
 // TODO Define the relationships between the models
 
+Location.belongsToMany(Traveller, {
+    through: Trip,
+});
 
-
+Traveller.belongsToMany(Location, {
+    through: Trip,
+})
 
 module.exports = { Traveller, Location, Trip };
