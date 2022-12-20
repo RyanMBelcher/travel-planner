2ba764d26b963b03909e0dfa278c327052576576
Ryan Belcher <ryanbelcher@ryans-air.myfiosgateway.com>
2022-12-20 10:57:47 -0500

    added location model

--- Develop/models/Location.js
+++ Develop/models/Location.js
@@ -2,12 +2,24 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // create our Location model
-class Location extends Model {}
+class Location extends Model { }
 
 // create fields/columns for Location model
 Location.init(
   {
-   //  TODO Add necessary columns, validators, and constraints
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    location_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlpha: true
+      }
+    }
   },
   {
     sequelize,
