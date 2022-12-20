6e6100d0e36430cc3c3909fb35b058f71edb2aa3
Ryan Belcher <ryanbelcher@ryans-air.myfiosgateway.com>
2022-12-20 11:04:20 -0500

    added trip model

--- Develop/models/Trip.js
+++ Develop/models/Trip.js
@@ -2,12 +2,39 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // create our Trip model
-class Trip extends Model {}
+class Trip extends Model { }
 
 // create fields/columns for Trip model
 Trip.init(
   {
-   //  TODO Add necessary columns, validators, and constraints
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    trip_budget: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    traveller_amount: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+    traveller_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'traveller',
+        key: 'id'
+      }
+    },
+    location_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'location',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
